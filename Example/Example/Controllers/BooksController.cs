20a96b9a6991190f8217b81b1629cbceb7bf0d97
Glebati228 <nub.azazaza@mail.ru>
2019-06-02 20:21:46 +0600

    creating database
    Create simple database for example

---
+++ Example/Example/Controllers/BooksController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Example.Models;
+
+namespace Example.Controllers
+{
+    public class BooksController : Controller
+    {
+        // GET: Books
+        BookContext db = new BookContext();
+        public ActionResult Index()
+        {
+            //ViewBag.books = db.Books.ToList();
+            return View();
+        }
+    }
+}
